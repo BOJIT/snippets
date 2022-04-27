4f513551abcdda6d4d3d116b4c753c3164958152
BOJIT <35117353+BOJIT@users.noreply.github.com>
2022-04-27 11:51:42 +0100

    Add settings.json + more snippets

--- get.sh
+++ get.sh
@@ -2,7 +2,7 @@
 
 # ---------------------------------------------------------------------------- #
 
-BASE_URL="https://snippets.bojit.org/snippets"
+BASE_URL="https://snippets.bojit.org"
 
 LANGUAGES=(cplusplus javascript matlab shell)
 
@@ -11,10 +11,24 @@ INPUT_AUTHOR=""
 
 # ---------------------------------------------------------------------------- #
 
-function getSnippet() {
-	URL="$BASE_URL/$1.code-snippets"
-	FILE=".vscode/$1.code-snippets"
+function addGitignore() {
 	GITIGNORE=".gitignore"
+
+	# Add to .gitignore file if present
+	if [ -f "$GITIGNORE" ]; then
+		if grep -q "$1" "$GITIGNORE"
+		then
+			echo "$1 already added to .gitignore"
+		else
+			echo -en "\n$1" >> "$GITIGNORE"
+			echo "Added $1 to .gitignore"
+		fi
+	fi
+}
+
+function getSnippet() {
+	URL="$BASE_URL/snippets/$1.code-snippets"
+	FILE=".vscode/$1.code-snippets"
 
 	# Download File
 	echo "Fetching $URL"
@@ -23,16 +37,7 @@ function getSnippet() {
 	# Change author name
 	sed -i "s/###AUTHOR###/$AUTHOR/" $FILE
 
-	# Add to .gitignore file if present
-	if [ -f "$GITIGNORE" ]; then
-		if grep -q "$FILE" "$GITIGNORE"
-		then
-			echo "$FILE already added to .gitignore"
-		else
-			echo -en "\n$FILE" >> "$GITIGNORE"
-			echo "Added $FILE to .gitignore"
-		fi
-	fi
+	addGitignore $FILE
 
 	echo "---------------------------------------------------------------------"
 }
@@ -50,6 +55,32 @@ mkdir -p .vscode
 
 # Get name for substitution
 read -p "Enter Author Name (default = $AUTHOR): " INPUT_AUTHOR
+
+# Get settings.json?
+read -p "Do you want to override settings.json? (y/n) " yn
+
+echo "---------------------------------------------------------------------"
+
+case $yn in 
+y )
+	SETTINGS_URL="$BASE_URL/settings.json"
+	SETTINGS_FILE=".vscode/settings.json"
+
+	# Download File
+	echo "Fetching $SETTINGS_URL"
+	curl $SETTINGS_URL -s --output $SETTINGS_FILE
+	addGitignore $SETTINGS_FILE
+	;;
+n )
+	echo "Ignoring settings.json..."
+	;;
+* )
+	echo "invalid response - exiting system"
+	exit 1
+	;;
+esac
+
+echo "---------------------------------------------------------------------"
 
 if [[ $INPUT_AUTHOR != "" ]]
 then
@@ -74,4 +105,6 @@ case $1 in
 	;;
 esac
 
-# TODO get settings.json too?
+echo "Snippets Added: Restart VSCode for snippets to load."
+
+# ---------------------------------------------------------------------------- #
